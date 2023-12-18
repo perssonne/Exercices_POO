e794ce6620d331ba4b9548ceb8a304a37b3aa3e8
perssonne <sanchezvaleroe@educ.college-montreal.qc.ca>
2023-12-18 12:10:15 -0500

    MAJ

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Créé par Enzo Sanchez Valero
 #Créé le 14/12/23
 #Introduction à Arcade
-
+import random
 #Partie 2
 
 from random import randint
@@ -14,18 +14,18 @@ class NPC:
         de_quatre = randint(1, 6)
         attribut = 0
 
-        if de_un & de_deux & de_trois > de_quatre:
+        if de_un > de_quatre and de_deux > de_quatre and de_trois > de_quatre:
             attribut = de_un + de_deux + de_trois
-        elif de_un & de_deux & de_quatre > de_trois:
+        elif de_un > de_trois and de_deux > de_trois and de_quatre > de_trois:
             attribut = de_un + de_deux + de_quatre
-        elif de_un & de_trois & de_quatre > de_deux:
+        elif de_un > de_deux and de_trois > de_deux and de_quatre > de_deux:
             attribut = de_un + de_trois + de_quatre
-        elif de_deux & de_trois & de_quatre > de_un:
+        elif de_deux > de_un and de_trois > de_un and de_quatre > de_un:
             attribut = de_deux + de_trois + de_quatre
 
         return attribut
 
-    def __init__(self):
+    def __init__(self, nom, race, espece, profession):
         self.force = self.attribut_aleatoire()
         self.agilite = self.attribut_aleatoire()
         self.constitution = self.attribut_aleatoire()
@@ -41,6 +41,7 @@ class NPC:
         self.profession = profession
 
     def afficher_caracteristiques(self):
+        print('')
         print('Force:', self.force)
         print('Agilité:', self.agilite)
         print('Constitution:', self.constitution)
@@ -51,6 +52,7 @@ class NPC:
         print('Classe armure:', self.classe_armure)
         print('Nom:', self.nom)
         print('Race:', self.race)
+        print('Espece:', self.espece)
         print('HP:', self.hp)
         print('Profession:', self.profession)
 
@@ -62,19 +64,39 @@ class Kobold(NPC):
         pass
 
     def subir_dommage(self, dommage):
-        self.dommage = randint(1, 6)
+        if dommage >= self.classe_armure:
+            self.hp = self.hp - (dommage - self.classe_armure)
+
+        elif dommage < self.classe_armure:
+            pass
+
+        else:
+            pass
 
 class Hero(NPC):
     def __init__(self):
         super.__init__()
 
     def attaquer(self, cible):
-        pass
+        self.attaquer = randint(1,20)
+        if self.attaquer == 20:
+            pass
+
+        elif self.attaquer == 1:
+            print('Attaque ratée')
+
+        elif self.attaquer <= 19 and self.attaquer >= 2:
+            self.cible(random.randint(1,6))
+
+        else:
+            pass
 
     def subir_dommage(self, dommage):
         self.dommage = randint(1,6)
 
-npc = NPC()
+
+npc = NPC('nom', 'race', 'espece', 'profession')
 npc.afficher_caracteristiques()
 
-
+kobold = NPC('nom', 'race', 'espece', 'profession')
+kobold.afficher_caracteristiques()
